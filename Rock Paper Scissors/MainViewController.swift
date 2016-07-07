d9477fa51709136e9d2efb0827437e7541be741b
Nico Schuele -mini <nicoschuele@gmail.com>
2016-07-08 00:27:03 +0200

    Add the main view controller

---
+++ Rock Paper Scissors/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Nico Schuele on 08/07/16.
+//  Copyright © 2016 Nico Schuele. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()     
+    }
+
+    
+}
