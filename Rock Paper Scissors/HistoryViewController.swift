866751a0e36ca474298a30c428892e50a1fcd8a2
Nico Schuele -mini <nicoschuele@gmail.com>
2016-08-03 17:38:01 +0200

    Add the TableView and prototype cell

--- Rock Paper Scissors/HistoryViewController.swift
+++ Rock Paper Scissors/HistoryViewController.swift
@@ -8,30 +8,17 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Results.results.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-    */
 
+    
     @IBAction func closeHistory(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
